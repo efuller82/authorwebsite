ca8f864521d0c93e55b916b9d20b6b11f13759d0
Eric Fuller <efuller82@gmail.com>
2019-12-13 11:13:20 -0600

    blog posts showing on page on click

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,40 @@
-// $(document).ready(function () {
-//     var settings = {
-//         "async": true,
-//         "crossDomain": true,
-//         "url": "https://theysaidso.p.rapidapi.com/quote?query=software",
-//         "method": "GET",
-//         "headers": config
-//     }
+$(document).ready(function() {
+  //     var settings = {
+  //         "async": true,
+  //         "crossDomain": true,
+  //         "url": "https://theysaidso.p.rapidapi.com/quote?query=software",
+  //         "method": "GET",
+  //         "headers": config
+  //     }
+  //     $.ajax(settings).done(function (response) {
+  //         console.log(response);
+  //         $('#qotd').append(response.contents.quote)
+  //         $('#qotd-author').append(response.contents.author)
+  //     });
+});
 
-//     $.ajax(settings).done(function (response) {
-//         console.log(response);
-//         $('#qotd').append(response.contents.quote)
-//         $('#qotd-author').append(response.contents.author)
-//     });
-// })
+$('.get-blogs').click(function() {
+  const settings = {
+    async: true,
+    url: '/blogs/all',
+    method: 'GET',
+  };
+  $.ajax(settings).done(function(response) {
+    // console.log(response);
+    response.forEach(function(blog) {
+      const blogDate = `<div>Date: ${blog.date}</div>`;
+      const blogCategory = `<div>Category: ${blog.category}</div>`;
+      const blogTitle = `<div>Title: ${blog.title}</div>`;
+      const blogContent = `<div>Content: ${blog.content}</div>`;
+
+      $(
+        '<div class="results card">' +
+          blogDate +
+          blogCategory +
+          blogTitle +
+          blogContent +
+          '</div>'
+      ).appendTo('.blog-posts');
+    });
+  });
+});
